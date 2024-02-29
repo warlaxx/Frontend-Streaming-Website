8315a25aa7c684af9ac60a45e0aee346de2ba2be
Abdallah Abdallaoui-maane <abdallah42.2001@icloud.com>
2024-02-29 15:34:32 +0100

    setup page maintenance

--- src/pages/ComingSoon.vue
+++ src/pages/ComingSoon.vue
@@ -1,21 +1,9 @@
 <template>
-  <div
-    class="coming-soon pt-32 flex flex-col items-center justify-center h-screen"
-  >
+  <div class="coming-soon flex flex-col items-center justify-center h-screen">
     <h1 class="text-4xl font-bold mb-4">Coming Soon</h1>
     <p class="text-lg mb-8">
       This page is currently under construction. Stay tuned for updates!
     </p>
-    <img
-      src="https://starwalk.space/gallery/images/what-is-space/1920x1080.jpg"
-      alt="Coming Soon"
-      class="max-w-sm mb-8"
-    />
-    <a
-      href="/"
-      class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-      >Go Back Home</a
-    >
   </div>
 </template>
 
@@ -28,6 +16,10 @@ export default {
 <style>
 /* Ajoutez vos styles personnalisés ici */
 .coming-soon {
-  background-color: #f3f4f6; /* Choisissez une couleur de fond */
+  background-image: url("@/assets/img/bg-comingsoon.jpg"); /* Remplacez par le lien de votre image */
+  background-size: cover; /* Couvrir toute la taille de l'écran */
+  background-position: center; /* Centrer l'image */
+  background-repeat: no-repeat; /* Ne pas répéter l'image */
+  color: white; /* Changez la couleur du texte pour améliorer la visibilité */
 }
 </style>
