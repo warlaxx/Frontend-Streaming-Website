b7998c70cac2b7b6d622879b9b6d18b05db556ad
Abdallah Abdallaoui-maane <abdallah42.2001@icloud.com>
2024-03-02 12:16:35 +0100

    navbar setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <!-- <NavbarMenu /> -->
+  <NavbarMenu />
   <router-view></router-view>
 </template>
 
 <script>
+import NavbarMenu from "@/components/NavbarMenu.vue";
 export default {
   name: "App",
-  components: {},
+  components: { NavbarMenu },
 };
 </script>
